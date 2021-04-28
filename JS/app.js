7bc6303e9527105e0569da3c1df4c1eca065f6db
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-04-28 14:24:02 +0300

    added Function to mayada project

--- JS/app.js
+++ JS/app.js
@@ -8,6 +8,8 @@ console.log(userName);
 let x =confirm('Nice to Meet You ' + userName + ' We Will Play a Game,'
 +' Please Answer with Yes/No OR  Y/N  ok?');
 
+function qusetion1(){
+
 let color = prompt('Do you think I like Purple Color?');
 console.log(color);
 
@@ -27,7 +29,10 @@ case 'N':
 alert('Wrong, I like Purple');
     break;
 }
+}
+qusetion1();
 
+function qusetion2(){
 
 let z= prompt('Do you think I Like reading books?');
 console.log(z);
@@ -47,8 +52,11 @@ switch (z.toUpperCase()){
         alert('You Wrong, I Like Reading Books');
        break;
 }
+}
+qusetion2();
 
 
+function qusetion3(){
 
 let movies = prompt('ok Do you think I Like Frozen Movie?');
 console.log(movies);
@@ -69,6 +77,10 @@ case 'NO':
 
     break;
 }
+}
+qusetion3();
+
+function qusetion4(){
 
 let fruit= prompt('What about Fruits, Do You think i like Apple?');
 console.log(fruit);
@@ -88,6 +100,10 @@ switch (fruit.toUpperCase()){
 alert('no, i like apple it has a Good Taste!');
        break;
 }
+}
+qusetion4();
+
+function qusetion5(){
 
 let like= prompt('Do you think I like coffee?');
 console.log(like);
@@ -107,10 +123,14 @@ switch (like.toUpperCase()){
 alert('no I Like Coffee, coffee is delicious');
        break;
 }
+}
+qusetion5();
 
 //-------------------------//
 
 alert('Now answer with a Number, it\'s guessing a number game, ok?');
+
+function qusetion6(){
 
 let ss= alert('Can You Guess How Many times I eat in Ramadan Night?');
 
@@ -140,12 +160,14 @@ for (let i = 1; i <= 4; i++) {
     }}
 
     alert('actually i eat 3 times in Ramadan Night ');
-//----------------------------------------
+}
+qusetion6();
+    //----------------------------------------
 
 
 alert('Now Guess between 6 options of my Favorite Fast Meals, Enter Words Ok?');
 
-
+function qusetion7(){
 
 let myMeal = ['pizza', 'burger', 'pasta','chicken','fish','sushi'];
 console.log(myMeal);
@@ -168,5 +190,8 @@ if (userFood === myMeal[0] || userFood === myMeal[1] || userFood === myMeal[2] |
 
 }
 alert('my favorite fast meals : pizza, burger, pasta, chicken, fish, sushi');
+}
+qusetion7();
+
 console.log('Total score is ' + score);
 alert('I enjoyed with you '+ userName + ' Your Total Score is '+ score + ' , have a Nice day');
